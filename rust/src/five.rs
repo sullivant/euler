ab335223395210f3cea4b6fc729b1d4e13ea5706
Thomas Sullivan <thomas.l.sullivan@vumc.org>
2019-04-23 08:26:23 -0500

    step 5 done; using step_by function.

--- rust/src/five.rs
+++ rust/src/five.rs
@@ -2,35 +2,23 @@
 //
 // What is the smallest positive number that is evenly divisible by all
 // of the numbers from 1 to 20?
-//
-
-// Div by 2?
-// Div by 5?
-// Div by 7?
-// Div by 11?
-// Div by 13?
-// Div by 17?
-// Div by 19?
 
 pub fn run() {
-    println!("Running problem 5");
+    println!("Running problem 5.");
 
-    'outer: for i in 20i64..99999999999999 {
+    for x in (2520i64..999999999999).step_by(2520) {
+        // Start at 2520, stepping by 2520
         let mut c: i32 = 0;
-        if i % 100000 == 0 {
-            println!("Got to {}", i);
-        }
-
-        for n in 1..20 {
-            if i % n == 0 {
-                c += 1;
-            } else {
-                continue 'outer;
+        for d in [11, 12, 13, 14, 15, 16, 17, 18, 19, 20].iter() {
+            // Check primes
+            if x % d != 0 {
+                c = 1;
             }
         }
-        if c == 20 {
-            println!("{} is div by 1=20", i);
-            break 'outer;
+        // If c is still 0 here, we have a candidate
+        if c == 0 {
+            println!("Candidate: {}", x);
+            break;
         }
     }
 }
