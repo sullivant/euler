fe4170ba1b5e20f1a2b5e042176690638369d59c
Thomas Sullivan <sullivan.t@gmail.com>
2019-04-23 19:03:51 -0500

    working on prime gen writing to file.

--- shared/gen_primes/src/main.rs
+++ shared/gen_primes/src/main.rs
@@ -5,26 +5,44 @@
 //
 
 use std::env;
+use std::error::Error;
+use std::fs::File;
+use std::io::Write;
+use std::process;
+
 
 pub fn main() {
     let args: Vec<String> = env::args().collect();
     let n: u64 = args[1].parse().unwrap();
+    let upper_limit: u64 = 10;
+
+    // If supplied N is not prime, lets bail and check on why
+    if !is_prime(n) { println!("{} is not prime. check on that.", n); return;}
 
     println!("Generating primes after known value: {}", n);
 
     let file_path: String = String::from("/mnt/d/sandbox/euler/shared/primes.txt");
     println!("File Path: {}", file_path);
 
-    println!("Adding 100 more primes to the list.");
-    //for i in ((last_prime + 2)..(last_prime + 10)).step_by(2) {
-    for i in ((n + 2)..(n + 10)).step_by(2) {
-        println!("Next: {} prime? -> {}", i, is_prime(i));
+    let mut prime_vec: Vec<u64> = Vec::new();
+    for i in ((n + 2)..(n + upper_limit)).step_by(2) {
+	if is_prime(i) { prime_vec.push(i); }
+    }
+    if prime_vec.len() > 0 { 
+        println!("Got {} primes; writing to file", prime_vec.len());
+        
+        if let Err(err) = write_primes(prime_vec) {
+            eprintln!("{}", err);
+            process::exit(1);
+        }
+    } else {
+        println!("No primes found.");
     }
 }
 
 // Returns true or false if this number is prime
 pub fn is_prime(n: u64) -> bool {
-    println!("Checking: {}", n);
+    //println!("Checking: {}", n);
 
     // Gather the sum of the digits
     let digits: Vec<_> = n
@@ -58,3 +76,14 @@ pub fn is_prime(n: u64) -> bool {
 
     return true;
 }
+
+
+fn write_primes(nums: Vec<u64>) -> Result<(), Box<Error>> {
+    //let nums = vec![1.0, 3.14, 5.1, 10.6, 42.424242];
+    let strings: Vec<String> = nums.iter().map(|n| n.to_string()).collect();
+
+    let mut file = File::create("/tmp/foobar")?;
+    writeln!(file, "{}", strings.join(", "))?;
+    Ok(())
+}
+
