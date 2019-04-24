14767f99fbf0b84fe533c4de3cfadf32a02d8407
Thomas Sullivan <thomas.l.sullivan@vumc.org>
2019-04-24 13:34:32 -0500

    increased check range and updated primes.

--- shared/gen_primes/src/main.rs
+++ shared/gen_primes/src/main.rs
@@ -5,11 +5,11 @@
 //
 // It generates them by checking that the supplied number is prime first,
 // then walks up the next 1,000,000 numbers, checking each for primality
-// and if prime adds them to a vector.  
+// and if prime adds them to a vector.
 //
 // At the end, it prints that vector of primes to a tmp file for review
 // to be appended to file_path if approved
-// 
+//
 // It could probably be written to just append them to file_path, but
 // I wanted to "baby step" this process.
 //
@@ -20,14 +20,16 @@ use std::fs::File;
 use std::io::Write;
 use std::process;
 
-
 pub fn main() {
     let args: Vec<String> = env::args().collect();
     let n: u64 = args[1].parse().unwrap();
-    let upper_limit: u64 = 1000000;
+    let upper_limit: u64 = 10000000;
 
     // If supplied N is not prime, lets bail and check on why
-    if !is_prime(n) { println!("{} is not prime. check on that.", n); return;}
+    if !is_prime(n) {
+        println!("{} is not prime. check on that.", n);
+        return;
+    }
 
     println!("Generating primes after known value: {}", n);
 
@@ -36,11 +38,13 @@ pub fn main() {
 
     let mut prime_vec: Vec<u64> = Vec::new();
     for i in ((n + 2)..(n + upper_limit)).step_by(2) {
-	if is_prime(i) { prime_vec.push(i); }
+        if is_prime(i) {
+            prime_vec.push(i);
+        }
     }
-    if prime_vec.len() > 0 { 
+    if prime_vec.len() > 0 {
         println!("Got {} primes; writing to file", prime_vec.len());
-        
+
         if let Err(err) = write_primes(prime_vec) {
             eprintln!("{}", err);
             process::exit(1);
@@ -53,7 +57,9 @@ pub fn main() {
 
 // Returns true or false if this number is prime
 pub fn is_prime(n: u64) -> bool {
-    if n % 100001 == 0 { println!("Checking: {}", n); }
+    if n % 100001 == 0 {
+        println!("Checking: {}", n);
+    }
 
     // Gather the sum of the digits
     let digits: Vec<_> = n
@@ -88,7 +94,6 @@ pub fn is_prime(n: u64) -> bool {
     return true;
 }
 
-
 fn write_primes(nums: Vec<u64>) -> Result<(), Box<Error>> {
     let strings: Vec<String> = nums.iter().map(|n| n.to_string()).collect();
 
@@ -96,4 +101,3 @@ fn write_primes(nums: Vec<u64>) -> Result<(), Box<Error>> {
     writeln!(file, "{}", strings.join("\n"))?;
     Ok(())
 }
-
