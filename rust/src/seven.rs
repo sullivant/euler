4132dfbecd00054ad29a0b29caad39ca28e4d73a
Thomas Sullivan <thomas.l.sullivan@vumc.org>
2019-04-26 07:17:57 -0500

    comment cleanup.

--- rust/src/seven.rs
+++ rust/src/seven.rs
@@ -2,10 +2,12 @@
 //
 // What is the 10 001st prime number?
 //
+// shared/gen_primes was used to generate the list of primes, so for the
+// answer see shared/primes.txt and check the value on line 10,001.
+//
 
 use super::shared;
 
 pub fn run() {
     println!("Running problem 7.");
-
 }
