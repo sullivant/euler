18cbda607ec5bc162e8399037184d35a8174884a
Thomas Sullivan <thomas.l.sullivan@vumc.org>
2019-06-03 11:01:50 -0500

    15 was easy to calc, but writing code to do it, too.

--- rust/src/fifteen.rs
+++ rust/src/fifteen.rs
@@ -8,6 +8,32 @@
 // How many such routes are there through a 20×20 grid
 //
 
+// This is, basically, a calculation of permutations; right can be = 1, down can be 0.
+// Using the formula: C(n, r) = n! / (r! * (n - r)!)
+// We get C(4,2) = 4! / (2! * (4-2)!) = 6
+//
+// Doing this for a 20x20 grid (40 1's + 0's), we get C(40,20) = 40! / (20! * (40-20)!) =
+// 137846528820 (A big number...)
+
+// Lets calculat this via Rust, anyway.
+
+extern crate bigint;
+use bigint::U256;
+use factorial::Factorial;
+
 pub fn run() {
     println!("Running problem 15.");
+
+    let sides: u64 = 20;
+    println!("{} x {} = {} moves", sides, sides, sides * 2);
+
+    let n: u64 = sides * 2;
+    let r: u64 = sides;
+
+    println!(
+        "C({},{}) = {}",
+        n,
+        r,
+        (n.factorial() / (r.factorial() * (n - r).factorial()))
+    );
 }
